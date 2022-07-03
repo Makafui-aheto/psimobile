3b65fbe72e7abbd68ff18f89f84ae7e9e5dd80f1
Makafui-aheto <59707287+Makafui-aheto@users.noreply.github.com>
2022-07-03 19:58:36 +0000

    DetailedCourseView update

--- psimobile/View/DetailedCourseView .swift
+++ psimobile/View/DetailedCourseView .swift
@@ -8,112 +8,139 @@
 import SwiftUI
 
 struct DetailedCourseView_: View {
-    
+
     @EnvironmentObject var courseData: CourseDetailModel
     var animation: Namespace.ID
     @State private var istoggled = false
     //@State private var textColor = (Color(.systemBackground)==Color(.white)) ? Color(.black) : Color(.white)
     @Environment(\.colorScheme) var colorScheme
-    
-    
+
+
     var body: some View {
-        
+
         let author = "By:\t" + courseData.selectedCourse.courseAuthor
-        
-        if courseData.selectedCourse != nil{
+
+        if (courseData.selectedCourse != nil){
+
             ZStack{
-                
+
                 Rectangle()
-                    .opacity(0.3)
-                    .ignoresSafeArea()
-                
+                        .opacity(0.5)
+                        .ignoresSafeArea()
+                        .onTapGesture{
+                            withAnimation{
+                                courseData.showCourseDetails.toggle()
+                            }
+
+                        }
+
                 //Rectangle()
-                   // .foregroundColor(.white)
-                   // .frame(width:350, height:500)
-                    //.cornerRadius(10)
-                    
-                VStack(alignment:.leading){
-                    
-                    Image(courseData.selectedCourse.courseProfilePic)
-                        .resizable()
-                        .renderingMode(.original)
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width:350, height:180)
-                        .matchedGeometryEffect(id: courseData.selectedCourse.id, in: animation)
-                        .background(
-                            Color(.black)
-                                .opacity(0.1)
-                                .onTapGesture {
-                                    withAnimation {
-                                        courseData.selectedCourse=nil
-                                        courseData.showCourseDetails.toggle()
-                                        
-                                    }
-                                }
-                        
-                        
-                        )
-                        .padding(.init(top: 0, leading: 10, bottom: 20, trailing: 10))
-                        .cornerRadius(5)
-                        .overlay(
-                            
-                            Image(systemName:"play")
-                                .foregroundColor(.white)
-                        
-                        )
-                    
-                    Text("COURSE")
-                        .font(.none)
-                        .foregroundColor(.gray)
-                        .padding(.init(top: 0, leading: 30, bottom: 5, trailing: 10))
-                    
-                    Text(courseData.selectedCourse.courseTitle)
-                        .font(.title)
-                        .foregroundColor(colorScheme == .dark ? Color.white : Color.black)
-                        .padding(.init(top: 0, leading: 30, bottom: 5, trailing: 10))
-                    
-                    Text(author)
-                        .font(.none)
-                        .foregroundColor(.gray)
-                        .padding(.init(top: 0, leading: 30, bottom: 5, trailing: 10))
-                    
-                    Text(courseData.selectedCourse.courseDescription)
-                        .foregroundColor(.gray)
-                        .padding(.init(top: 0, leading: 30, bottom: 20, trailing: 10))
-                    
-                    Spacer()
-                    
-                    HStack{
-                        Text("Skills : Actuarial Science")
-                            .foregroundColor(.gray)
+                // .foregroundColor(.white)
+                // .frame(width:350, height:500)
+                //.cornerRadius(10)
+
+                VStack{
+
+                    VStack(alignment:.leading){
+
+                        Image(courseData.selectedCourse.courseProfilePic)
+                                .resizable()
+                                .renderingMode(.original)
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width:320, height:135)
+                                .matchedGeometryEffect(id: courseData.selectedCourse.id, in: animation)
+                                .background(
+                                        Color(.black)
+                                                .opacity(0.1)
+                                                .onTapGesture {
+                                                    withAnimation {
+                                                        courseData.selectedCourse=nil
+                                                        courseData.showCourseDetails.toggle()
+
+                                                    }
+                                                }
+
+
+                                )
+                                .padding(.init(top: 0, leading: 10, bottom: 20, trailing: 10))
+                                .cornerRadius(5)
+                                .overlay(
+
+                                        Image(systemName:"play")
+                                                .foregroundColor(.white)
+
+                                )
+
+                        Text("COURSE")
+                                .font(.none)
+                                .foregroundColor(.gray)
+                                .padding(.init(top: 0, leading: 30, bottom: 5, trailing: 10))
+
+                        Text(courseData.selectedCourse.courseTitle)
+                                .font(.title)
+                                .foregroundColor(colorScheme == .dark ? Color.white : Color.black)
+                                .padding(.init(top: 0, leading: 30, bottom: 5, trailing: 10))
+
+                        Text(author)
+                                .font(.system(size: 16, weight: .light, design: .default))
+                                .foregroundColor(.gray)
+                                .padding(.init(top: 0, leading: 30, bottom: 5, trailing: 10))
+
+                        Text(courseData.selectedCourse.courseDescription)
+                                .foregroundColor(.gray)
+                                .padding(.init(top: 0, leading: 30, bottom: 20, trailing: 10))
+
                         Spacer()
-                        
-                        Button(action: {istoggled = !istoggled}, label: {
-                            Image(systemName: (istoggled==false) ? "bookmark" : "bookmark.fill")
-                                .foregroundColor(.blue)
-                            Text("Save")
-                                .foregroundColor(.blue)
-                        })
-                        
+
+
+                        HStack{
+                            Text("Skills : Actuarial Science")
+                                    .foregroundColor(.gray)
+                            Spacer()
+
+                            Button(action: {istoggled = !istoggled}, label: {
+                                Image(systemName: (istoggled==false) ? "bookmark" : "bookmark.fill")
+                                        .foregroundColor(.blue)
+                                Text("Save")
+                                        .foregroundColor(.blue)
+                            })
+
+                        }
+                                .padding(.init(top: 0, leading: 30, bottom: 20, trailing: 10))
+
+
                     }
-                    .padding(.init(top: 0, leading: 30, bottom: 20, trailing: 10))
-               
-                    
-                    
+                            .frame(width:320, height:450)
+                            .foregroundColor(Color(UIColor.systemBackground))
+                            .background(Color(UIColor.systemBackground))
+                            .cornerRadius(10)
+
+                    Button(action:{
+                        withAnimation{
+                            courseData.showCourseDetails.toggle()
+                        }
+
+                    }){
+                        Image(systemName: "xmark")
+                                .resizable()
+                                .frame(width:12, height:12)
+                                .foregroundColor(.black)
+                                .padding(20)
+                    }
+                            .background(Color.white)
+                            .clipShape(Circle())
+                            .padding(.top, 10)
                 }
-                .frame(width:350, height:500)
-                .foregroundColor(Color(UIColor.systemBackground))
-                .background(Color(UIColor.systemBackground))
-                .cornerRadius(10)
-                
+
+
             }
-         
-            
+
+
         }
-        
-        
-        
-        
+
+
+
+
     }
 }
 
