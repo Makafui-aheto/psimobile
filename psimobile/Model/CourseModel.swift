22f624254dcd0df8192fbaa9612598fcae3252cb
Makafui-aheto <59707287+Makafui-aheto@users.noreply.github.com>
2023-02-13 07:37:16 +0000

    CourseModel update

--- psimobile/Model/CourseModel.swift
+++ psimobile/Model/CourseModel.swift
@@ -11,7 +11,7 @@ class CourseModel: ObservableObject{
 
     private func fetchData<T:Encodable>(for apiURL: String,
                                          completion: @escaping (Result<[CourseDTO], Error>) -> Void,
-                                         parameters: Dictionary<String, T>)-> [CourseDTO]{
+                                         parameters: Dictionary<String, T> = [:]) async -> [CourseDTO]{
 
         guard let apiURL = URL(string: apiURL)else{return courses}
 
@@ -40,8 +40,9 @@ class CourseModel: ObservableObject{
             if let data = data{
                 do{
                     let courses = try decoder.decode([CourseDTO].self, from: data)
-                    DispatchQueue.main.async{self?.courses = courses}
+                    self?.courses = courses
                     completion(.success(courses))
+
                 }catch let decoderError{
                     completion(.failure(decoderError))
                 }
@@ -55,26 +56,74 @@ class CourseModel: ObservableObject{
 
 
 
-//    private func postData<T: Decodable>(for apiURL: String,
-//                                        completion: @escaping ()->Void , parameters: [T],
-//                                        image: [UIImage]?){
-//
-//        guard let apiURL = URL(string: apiURL)else{return}
-//        var urlRequest = URLRequest(url: apiURL)
-//        let session = URLSession.shared
-//
-//        urlRequest.httpMethod = "Post"
-//
-//        forEach(object){ object in
-//
-//        }
-//
-//    }
+    func postData<T: Codable>(for apiURL: String,
+                                        completion: @escaping ()->Void , parameters: Dictionary<String, T>? = [:],
+                                        object: [Any] = [],
+                                        images: Dictionary<String, UIImage> = [:]) async throws -> Any{
+
+        guard let apiURL = URL(string: apiURL)else{return "failed"}
+        var components = URLComponents(url: apiURL, resolvingAgainstBaseURL: false)!
+
+        let boundary = UUID().uuidString
+
+        let session = URLSession.shared
+
+        var data = Data()
+
+        var queryItems:[URLQueryItem] = []
+
+       Array(parameters!.keys).map{ element in
+           queryItems.append(URLQueryItem(name: element, value: parameters![element] as? String))
+       }
+
+        components.queryItems = queryItems
+
+        var urlRequest = URLRequest(url: components.url!)
+
+        urlRequest.httpMethod = "Post"
+
+        urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        urlRequest.addValue("application/json", forHTTPHeaderField: "Accept")
+
+        urlRequest.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+
+        data.append("\r\n--\(boundary)\r\n".data(using: .utf8)!)
+        let paramName = "file"
+
+        object.map{data.append(try! JSONSerialization.data(withJSONObject: $0, options: []))}
+
+        for (key, value) in images{
+            data.append(value.pngData()!)
+            data.append("""
+                        Content-Disposition: form-data; name=\"\(paramName)\"; 
+                        filename=\"\(key)\"\r\n
+                        """.data(using: .utf8)!)
+        }
+
+        data.append("Content-Type: image/png\r\n\r\n".data(using: .utf8)!)
+        data.append("\r\n--\(boundary)--\r\n".data(using: .utf8)!)
 
 
-    func getCoursesByUploadDate(uploadDate: Date, apiURL: String) -> [CourseDTO]{
+        let uploadtask = session.uploadTask(with: urlRequest,
+                from: data, completionHandler: <#T##@escaping @Sendable (Data?, URLResponse?, Error?) -> Void##@escaping @Sendable (Foundation.Data?, Foundation.URLResponse?, Swift.Error?) -> Swift.Void#>){ (data, response, error) in
 
-        self.fetchData(for: apiURL, completion: { (result) in
+            if let error = error{completion(.failure(error))}
+
+            let encoder = JSONEncoder
+
+
+        }
+
+
+        return "Success"
+
+    }
+
+
+    func getCoursesByUploadDate(uploadDate: Date, apiURL: String) async -> [CourseDTO]{
+
+        await self.fetchData(for: apiURL, completion: { (result) in
             switch result {
             case .success:
                     print("done")
@@ -84,8 +133,8 @@ class CourseModel: ObservableObject{
         },parameters: ["uploadDate": uploadDate])
     }
 
-    func getCoursesByAuthor(author: String,apiURL:String) -> [CourseDTO]{
-        self.fetchData(for: apiURL,completion: { (result) in
+    func getCoursesByAuthor(author: String,apiURL:String) async -> [CourseDTO]{
+        await self.fetchData(for: apiURL,completion: { (result) in
             switch result {
             case .success:
                 print("done")
@@ -96,8 +145,8 @@ class CourseModel: ObservableObject{
 
     }
 
-    func getCoursesByTopic(topic: String, apiURL:String)-> [CourseDTO]{
-        self.fetchData(for:apiURL,completion: { (result) in
+    func getCoursesByTopic(topic: String, apiURL:String) async -> [CourseDTO]{
+        await self.fetchData(for:apiURL,completion: { (result) in
             switch result {
             case .success:
                 print("done")
