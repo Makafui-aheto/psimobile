de2ac1ac286eaf259ed3699ecefb1da1fbdcb783
Makafui-aheto <59707287+Makafui-aheto@users.noreply.github.com>
2023-02-13 07:42:36 +0000

    CourseModel update

---
+++ psimobile/View/CourseTile.swift
@@ -0,0 +1,20 @@
+//
+// Created by Makafui Aheto on 13/02/2023.
+//
+
+import Foundation
+
+import SwiftUI
+
+struct CourseTile: View {
+
+    @EnvironmentObject var courseModel: CourseModel
+
+    var body: some View{
+
+
+
+
+    }
+
+}
